c2317e0aee5da0377619f6561b5473925bd0edae
Arthur Guiot <arguiot@gmail.com>
2018-07-21 22:01:41 +0200

    Designing the cells

---
+++ Timeline/Table View/TodoCell.swift
@@ -0,0 +1,17 @@
+//
+//  TodoCell.swift
+//  Timeline
+//
+//  Created by Arthur Guiot on 21/7/18.
+//  Copyright © 2018 Arthur Guiot. All rights reserved.
+//
+
+import UIKit
+
+class TodoCell: UITableViewCell {
+    
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var desc: UILabel!
+    @IBOutlet weak var date: UILabel!
+    
+}
