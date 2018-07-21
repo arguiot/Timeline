fe7abc4f2688253836e0818f73d315f205173858
Arthur Guiot <arguiot@gmail.com>
2018-07-21 19:01:32 +0200

    Started designing top bar

---
+++ Timeline/Views/TopBar.swift
@@ -0,0 +1,45 @@
+//
+//  TopBar.swift
+//  Timeline
+//
+//  Created by Arthur Guiot on 21/7/18.
+//  Copyright © 2018 Arthur Guiot. All rights reserved.
+//
+
+import UIKit
+
+class TopBar: UIView {
+    @IBOutlet var contentView: UIView!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        Bundle.main.loadNibNamed("TopBar", owner: self, options: nil)
+        
+        addSubview(contentView)
+        contentView.frame = self.bounds
+        contentView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        
+        contentView.layer.shadowColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0.2513479313)
+        contentView.layer.shadowOpacity = 1
+        contentView.layer.shadowOffset = CGSize(width: 0, height: 5)
+        contentView.layer.shadowRadius = 20
+        contentView.layer.shouldRasterize = true
+    }
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
