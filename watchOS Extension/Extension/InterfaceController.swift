6df12c787b1cc6b7a25c555997922a59651ddbac
Arthur Guiot <arguiot@gmail.com>
2018-08-07 20:49:21 +0200

    Cheating (cause Simulator doesn't support iCloud operations

--- watchOS Extension/Extension/InterfaceController.swift
+++ watchOS Extension/Extension/InterfaceController.swift
@@ -41,46 +41,50 @@ class InterfaceController: WKInterfaceController {
         // Last created first
         query.sortDescriptors = [NSSortDescriptor(key: "initDate", ascending: false)]
         
-        db.perform(query, inZoneWith: nil) { (records, error) in
-            if error != nil {
-                print("Log: \(String(describing: error))")
-            }
-            
-            guard let records = records else { return }
-            self.todos = [] // Emptying todos
-            for record in records {
-                self.todos.append(ToDos(name: record.value(forKey: "name") as! String,
-                                        desc: record.value(forKey: "desc") as! String,
-                                        date: record.value(forKey: "date") as! Date,
-                                        initDate: record.value(forKey: "initDate") as! Date,
-                                        record: record.recordID))
-            }
-            DispatchQueue.main.sync {
-               completionHandler(self.todos)
-            }
-        }
+//        db.perform(query, inZoneWith: nil) { (records, error) in
+//            if error != nil {
+//                print("Log: \(String(describing: error))")
+//            }
+//
+//            guard let records = records else { return }
+//            self.todos = [] // Emptying todos
+//            for record in records {
+//                self.todos.append(ToDos(name: record.value(forKey: "name") as! String,
+//                                        desc: record.value(forKey: "desc") as! String,
+//                                        date: record.value(forKey: "date") as! Date,
+//                                        initDate: record.value(forKey: "initDate") as! Date,
+//                                        record: record.recordID))
+//            }
+//            DispatchQueue.main.sync {
+//               completionHandler(self.todos)
+//            }
+//        }
+        completionHandler([ToDos(name: "test", desc: "test", date: Date(timeIntervalSince1970: 1534629600), initDate: Date(timeIntervalSince1970: 1533667446), record: nil)])
     }
     func loadTodos() {
         fetchItems { (data) in
             self.table.setNumberOfRows(data.count, withRowType: "Todo")
             
             let rowCount = self.table.numberOfRows
-            for i in 0...rowCount {
-                let row = self.table.rowController(at: i) as! Cell
-                
-                row.name.setText(data[i].name)
-                
-                // compute progress
-                let d1d2 = Float(
-                    data[i].date.timeIntervalSince((data[i].initDate))
-                )
-                let nowd2 = Float(
-                    Date().timeIntervalSince(data[i].initDate)
-                )
-                let p = Int((nowd2 / d1d2) * 100)
-                
-                row.progress.setText(String(p))
-                
+            if rowCount > 0 {
+                for i in 0...rowCount - 1{
+                    let c = self.table.rowController(at: i)
+                    let row = c as! Cell
+                    
+                    row.name.setText(data[i].name)
+                    
+                    // compute progress
+                    let d1d2 = Float(
+                        data[i].date.timeIntervalSince((data[i].initDate))
+                    )
+                    let nowd2 = Float(
+                        Date().timeIntervalSince(data[i].initDate)
+                    )
+                    let p = Int((nowd2 / d1d2) * 100)
+                    
+                    row.progress.setText(String(p))
+                    
+                }
             }
         }
     }
