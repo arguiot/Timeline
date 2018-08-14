547152bd15d1b5f7512d0862ef7a26d50ce78086
Arthur Guiot <arguiot@gmail.com>
2018-08-14 11:48:07 +0200

    Todos rendering (web app)

--- docs/js/todos.js
+++ docs/js/todos.js
@@ -113,7 +113,61 @@ class TodosGroup extends P.Group {
 	render(todos) {
 		for (let i = 0; i < todos.length; i++) {
 			const fields = todos[i].fields
+
+			const date = new Date(fields.date.value)
+			const model = `
+			<div class="todo">
+				<div class="date">${this.dateFormat(date, "D, dd MMM HH:mm")}</div>
+				<h1>${fields.name.value}</h1>
+				<p>${fields.desc.value}</p>
+				<div class="progress"><div class="bar"></div></div>
+			</div>
+			`
+			this.group.insertAdjacentHTML("beforeend", model);
 		}
+	}
+
+	dateFormat(date, format) {
+		var monthNames = [
+			"January", "February", "March",
+			"April", "May", "June", "July",
+			"August", "September", "October",
+			"November", "December"
+		];
+
+		var Days = [
+			"Sunday", "Monday", "Tuesday", "Wednesday",
+			"Thursday", "Friday", "Saturday"
+		];
+
+		function formatDate(dt, format) {
+			format = format.replace('ss', pad(dt.getSeconds(), 2));
+			format = format.replace('s', dt.getSeconds());
+			format = format.replace('dd', pad(dt.getDate(), 2));
+			format = format.replace('d', dt.getDate());
+			format = format.replace('mm', pad(dt.getMinutes(), 2));
+			format = format.replace('m', dt.getMinutes());
+			format = format.replace('MMMM', monthNames[dt.getMonth()]);
+			format = format.replace('MMM', monthNames[dt.getMonth()].substring(0, 3));
+			format = format.replace('MM', pad(dt.getMonth() + 1, 2));
+			format = format.replace(/M(?![ao])/, dt.getMonth() + 1);
+			format = format.replace('DD', Days[dt.getDay()]);
+			format = format.replace(/D(?!e)/, Days[dt.getDay()].substring(0, 3));
+			format = format.replace('yyyy', dt.getFullYear());
+			format = format.replace('YYYY', dt.getFullYear());
+			format = format.replace('yy', (dt.getFullYear() + "").substring(2));
+			format = format.replace('YY', (dt.getFullYear() + "").substring(2));
+			format = format.replace('HH', pad(dt.getHours(), 2));
+			format = format.replace('H', dt.getHours());
+			return format;
+		}
+
+		function pad (n, width, z) {
+			z = z || '0';
+			n = n + '';
+			return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+		}
+		return formatDate(date, format)
 	}
 }
 P.autoMount(MainView)
